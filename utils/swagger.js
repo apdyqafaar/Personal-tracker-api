08902509087cf3c0c7f9f3c3ab1f8ab9748abb34
apdiqafaar <qudhacle43@gmail.com>
2025-07-11 16:55:47 +0300

    pro

--- utils/swagger.js
+++ utils/swagger.js
@@ -4,7 +4,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 
-const __filename = fileURLToPath(process.env.NODE_ENV =='development'?import.meta.url :'https://personal-tracker-api-z0z2.onrender.com');
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml'));
