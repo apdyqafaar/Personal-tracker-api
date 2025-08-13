b11f1bc79e087bb5288e5dc6d0149e9638ff2629
apdiqafaar <qudhacle43@gmail.com>
2025-08-13 13:44:27 +0300

    f

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
   
   app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    res.send(path.join(__dirname, "..","frontend","dist", "index.html"));
   });
 }
 
